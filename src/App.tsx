12d3353f5904e393cf8b1886df2a0b6f6dcd1bb4
edcabralc <cabralnet@gmail.com>
2025-04-03 11:15:23 -0300

    style: adicionando elementos no background

--- src/App.tsx
+++ src/App.tsx
@@ -11,30 +11,28 @@ const App = () => {
   const { data, loading, error, getUser, resetSearch } = useData();
 
   return (
-    <>
-      <main>
-        <Container>
-          <Header />
-          <Form action={getUser} resetAction={resetSearch} />
+    <main>
+      <Container>
+        <Header />
+        <Form action={getUser} resetAction={resetSearch} />
 
-          {error && (
-            <Card>
-              <Error error={error} />
-            </Card>
-          )}
-          {!data && loading && (
-            <Card>
-              <Loading />
-            </Card>
-          )}
-          {data && (
-            <Card>
-              <User user={data} />
-            </Card>
-          )}
-        </Container>
-      </main>
-    </>
+        {error && (
+          <Card>
+            <Error error={error} />
+          </Card>
+        )}
+        {!data && loading && (
+          <Card>
+            <Loading />
+          </Card>
+        )}
+        {data && (
+          <Card>
+            <User user={data} />
+          </Card>
+        )}
+      </Container>
+    </main>
   );
 };
 
